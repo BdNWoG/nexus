a9fdf767f18fceb193ab18bfca2cd743a2f1619d
BdNWoG <billy.gao@hotmail.com>
2024-06-16 06:35:00 +0800

    app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+
+import App from "./App";
+import "./index.css";
 
 //@ts-ignore
 const root = ReactDOM.createRoot(document.getElementById("root"));
