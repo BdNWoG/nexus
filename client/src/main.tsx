7c69b4935411b417e83cf47f9425a1d24062ad6a
BdNWoG <billy.gao@hotmail.com>
2024-06-16 06:17:11 +0800

    css

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,9 +4,11 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
+//@ts-ignore
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
+  //@ts-ignore
   <ThirdwebProvider desiredChainId={ChainId.Sepolia}>
     <Router>
       <App />
