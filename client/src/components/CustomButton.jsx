c42e8da65d02c25403c7c5d9f28e192025d39daf
BdNWoG <billy.gao@hotmail.com>
2024-06-16 08:36:51 +0800

    nav

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const CustomButton = () => {
+const CustomButton = ({ 
+    btnType, title, handleClick, styles 
+}) => {
     return (
-        <div>CustomButton</div>
+        <button type={btnType} className={`${styles} font-epilogue font-semibold leading-[26px] test-[16px] text-white min-h-[52px] px-4 rounded-[10px]`} onClick={handleClick}>
+            {title}
+        </button>
     )
 }
 
