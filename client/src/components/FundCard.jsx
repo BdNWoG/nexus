116c5b513d5a5a88a2048b12793487253f3055aa
BdNWoG <billy.gao@hotmail.com>
2024-06-18 10:05:57 +0800

    card

---
+++ client/src/components/FundCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { tagType, thirdweb } from '../assets'
+import { daysLeft } from '../utils'
+
+const FundCard = ({
+    owner, title, description, target, deadline, amountCollected, image, handleClick
+}) => {
+    const remainingDays = daysLeft(deadline)
+
+    return (
+        <div>FundCard</div>
+    )
+}
+
+export default FundCard
